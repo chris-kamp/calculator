3c7006b59e1d22b7f94bb6b547cef1b9eff41882
Chris Kamp <chriskamp101@gmail.com>
2020-09-05 10:29:41 +1000

    Enable basic mathematical operations

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//Variables to store the first argument, second argument and operation to be performed
+const args = {
+    firstArg: "0",
+    secondArg: null,
+    operation: null
+}
+
+
 //add two given numbers
 function add(a, b) {
     return a + b;
@@ -27,13 +35,125 @@ function divide(a, b) {
 //given two numbers and an operator, perform the relevant operation on the two numbers
 function operate(a, b, operator) {
     switch(operator) {
-        case "+":
+        case "add":
             return add(a, b);
-        case "-":
+        case "subtract":
             return subtract(a, b);
-        case "*":
+        case "multiply":
             return multiply(a, b);
-        case "/":
+        case "divide":
             return divide(a, b);
     }
 }
+
+//A string that will be shown in the calculator display
+let displayString = "0";
+
+//An array to queue numbers and operations
+let queue = [];
+
+//An object containing a dictionary of IDs and what they should output
+let idToOutput = {
+    n0: "0",
+    n1: "1",
+    n2: "2",
+    n3: "3",
+    n4: "4",
+    n5: "5",
+    n6: "6",
+    n7: "7",
+    n8: "8",
+    n9: "9",
+    add: "\u002B",
+    subtract: "\u2212",
+    multiply: "\u00D7",
+    divide: "\u00F7",
+};
+
+//Get an array containing all of the number buttons and add an event listener
+const numButtons = Array.from(document.querySelectorAll(".numButton"));
+
+numButtons.forEach(button => {
+    button.addEventListener("click", pushNumber);
+})
+
+//Get an array containing the operator buttons and add an event listener
+const opButtons = Array.from(document.querySelectorAll(".opButton"));
+opButtons.forEach(button => {
+    button.addEventListener("click", pushOperator);
+});
+
+
+//Clear button clears display on click
+const clearButton = document.querySelector("#clear");
+clearButton.addEventListener("click", clear);
+
+const display = document.querySelector("#display");
+
+//A function to update the display
+function updateDisplay() {
+    displayString = args.firstArg;
+    if(args.operation) {displayString += idToOutput[args.operation]}; //This will need to be the symbol, not the name of the operation
+    if(args.secondArg) {displayString += args.secondArg};
+    display.textContent = displayString;
+}
+
+//A function to clear the display
+function clear() {
+    args.firstArg = "0";
+    args.secondArg = null;
+    args.operation = null;
+    updateDisplay();
+}
+
+//An event listener function to append a number to the display when
+//its key is pressed
+function pushNumber(e) {
+    const arg = args.operation ? "secondArg" : "firstArg";
+    const number = idToOutput[e.target.id];
+    if (args[arg] === "0" || args[arg] === null) {
+        if(number !== "0") {
+            args[arg] = number; //Needs to refuse leading zero in second arg
+        }
+    } else {
+        args[arg] += number;
+    }
+    updateDisplay();
+}
+
+//An event listener function to append an operator to the display (after performing an operation on the current arguments if all are present)
+function pushOperator(e) {
+    const operator = e.target.id;
+    if(args.secondArg) {
+        const answer = operate(+args.firstArg, +args.secondArg, args.operation);
+        args.firstArg = answer;
+        args.secondArg = null;
+    }
+        args.operation = operator;
+        updateDisplay();
+}
+
+//A function to run 
+
+
+/* 
+?Need to deal with situation where equals has been pressed and there is an answer present. New presses do what - replace?
+
+Display string is initially 0
+When you press a number key:
+    -If "operation" is truthy, update the second arg, otherwise update the first arg
+    -If the arg is "0", replace the arg with the number pressed
+    -If the arg is null, replace the arg with the number pressed, unless the number pressed is 0, in which case do nothing
+    -Otherwise, append the number pressed to the arg
+    -Then, update the display
+When you press an operator key:
+    -If second arg is truthy (which implies there is a first arg and an operator already):  
+        -Operate on first arg, second arg, operator
+        -Set first arg equal to the result
+        -Set second arg to null
+        -Set operation to the pressed operator key
+        -Update the display
+    -If second arg is falsy:
+        -Set operation to the pressed operator key
+        -Update the display
+*/
