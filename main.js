1e4a705e5ea265c85b05e3e4b9f73e9ccf985154
Chris Kamp <chriskamp101@gmail.com>
2020-09-08 20:54:52 +1000

    Refactor code for readability and remove redundant code

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-//Variables to store the first argument, second argument, operation to be performed and final resulting answer.
+//The maximum string length of an argument or answer
+const MAX_ARG_LENGTH = 14;
+
+//The string shown in the calculator display
+let displayString = "0";
+
+//An array containing all of the buttons on the page
+const buttons = Array.from(document.querySelectorAll("button"));
+
+//The element in which numbers are displayed
+const display = document.querySelector("#display");
+
+//An object to store the first argument, second argument, operation to be performed and final resulting answer.
 const args = {
     firstArg: "0",
     secondArg: null,
@@ -6,56 +18,7 @@ const args = {
     answer: null
 }
 
-//The maximum string length of an argument
-const MAX_ARG_LENGTH = 14;
-
-//add two given numbers
-function add(a, b) {
-    return a + b;
-}
-
-//for two given numbers, subtract the second from the first
-function subtract(a, b) {
-    return a - b;
-}
-
-//multiply two given numbers
-function multiply(a, b) {
-    return a * b;
-}
-
-//for two given numbers, divide the first by the second
-//division by zero gives an error
-function divide(a, b) {
-    if(b === 0) {
-        return "ERROR (Divide by 0)"
-    }
-    else {
-        return a / b;
-    }
-}
-
-//given two numbers and an operator, perform the relevant operation on the two numbers
-function operate(a, b, operator) {
-    switch(operator) {
-        case "add":
-            return add(a, b);
-        case "subtract":
-            return subtract(a, b);
-        case "multiply":
-            return multiply(a, b);
-        case "divide":
-            return divide(a, b);
-    }
-}
-
-//A string that will be shown in the calculator display
-let displayString = "0";
-
-//An array to queue numbers and operations
-let queue = [];
-
-//An object containing a dictionary of IDs and what they should output
+//A dictionary of button IDs and what they should output when activated
 let idToOutput = {
     n0: "0",
     n1: "1",
@@ -73,7 +36,7 @@ let idToOutput = {
     divide: "\u00F7",
 };
 
-//An object containing a dictionary of keys and the ID of the associated button
+//A dictionary of keyboard keys and the ID of the button to activate when each is pressed
 let keyToID = {
     "0": "n0",
     "1": "n1",
@@ -95,22 +58,66 @@ let keyToID = {
     "Escape": "clear",
 }
 
-//Get an array containing all of the number buttons and add event listeners
-const numButtons = Array.from(document.querySelectorAll(".numButton"));
+//Functions to perform mathematical operations on two given arguments
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if(b === 0) {
+        return "ERROR (Divide by 0)"
+    }
+    else {
+        return a / b;
+    }
+}
 
-numButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        pushButton(e.target.id);
-    });
-})
+//Given two numbers and an operator, perform the relevant operation on the two numbers
+function operate(a, b, operator) {
+    switch(operator) {
+        case "add":
+            return add(a, b);
+        case "subtract":
+            return subtract(a, b);
+        case "multiply":
+            return multiply(a, b);
+        case "divide":
+            return divide(a, b);
+    }
+}
 
-//Add an event listener for keypresses
+//Update the display
+function updateDisplay() {
+    if(args.answer) {
+        displayString = args.answer;
+    } else {
+        displayString = args.firstArg;
+        if(args.operation) {
+            displayString += idToOutput[args.operation]
+        }; 
+        if(args.secondArg) {
+            displayString += args.secondArg
+        };
+    }
+    display.textContent = displayString;
+}
+
+//Clear all parameters and reset the calculator to its base state
+function clear() {
+    args.firstArg = "0";
+    args.secondArg = null;
+    args.operation = null;
+    args.answer = null;
+}
+
+//When a key is pressed, call pushButton on the ID of the associated button (if any)
 window.addEventListener("keydown", keyToButton);
 
-//Get an array containing all buttons
-const buttons = Array.from(document.querySelectorAll("button"));
-
-//A function to get a button from a key press
 function keyToButton(e) {
     if(keyToID[e.key]) {
         const id = keyToID[e.key];
@@ -118,8 +125,14 @@ function keyToButton(e) {
     }
 }
 
-//A function to activate a button given its ID
-//TODO: Refactor this with an object
+//When a button is clicked, call pushButton on its ID
+buttons.forEach(button => {
+    button.addEventListener("click", e => {
+        pushButton(e.target.id);
+    })
+})
+
+//Given a button ID, perform its associated operations
 function pushButton(id) {
     const button = document.getElementById(id);
     if(button.classList.contains("numButton")) {
@@ -137,62 +150,16 @@ function pushButton(id) {
     }
 }
 
-//Get an array containing the operator buttons and add an event listener
-const opButtons = Array.from(document.querySelectorAll(".opButton"));
-opButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        pushButton(e.target.id);
-    });
-});
-
-
-
-//A function to convert an answer to an argument to be operated upon
-function answerToArg() {
-    const answer = args.answer;
-    clear();
-    args.firstArg = answer.toString();
-}
-
-//Get a reference to the display field
-const display = document.querySelector("#display");
-//A function to update the display
-function updateDisplay() {
-    if(args.answer) {
-        displayString = args.answer;
-    } else {
-        displayString = args.firstArg;
-        if(args.operation) {
-            displayString += idToOutput[args.operation]
-        }; 
-        if(args.secondArg) {
-            displayString += args.secondArg
-        };
-    }
-    display.textContent = displayString;
-}
-
-//A function to clear all args
-function clear() {
-    args.firstArg = "0";
-    args.secondArg = null;
-    args.operation = null;
-    args.answer = null;
-}
-
-//An event listener function to append a number to the display when its key is pressed
+//When a number button is activated, push it to the relevant argument and update the display.
 function pushNumber(id) {
-    //If there is an answer, it should be cleared and replaced with the number pressed
     if(args.answer) {
         clear();
     }
     const arg = args.operation ? "secondArg" : "firstArg";
     const number = idToOutput[id];
-    //If the arg is equal to 0 or is null, set the arg equal to the number pressed.
     if(args[arg] === "0" || args[arg] === null) {
         args[arg] = number;
     } else {
-        //Do not allow args longer than MAX_ARG_LENGTH characters
         if(args[arg].length >= MAX_ARG_LENGTH) {
             return;
         }
@@ -201,75 +168,8 @@ function pushNumber(id) {
     updateDisplay();
 }
 
-//An event listener function to append an operator to the display (after performing an operation on the current arguments if all are present)
-function pushOperator(id) {
-    //If there is an answer, it should be converted to an arg to be operated upon
-    if(args.answer) {
-        answerToArg();
-    }
-    const operator = id;
-    if(args.secondArg) {
-        args.secondArg = clearTrailingDecimal(args.secondArg);
-        const answer = roundAnswer(operate(+args.firstArg, +args.secondArg, args.operation));
-        args.firstArg = answer.toString();
-        args.secondArg = null;
-    } else if (args.firstArg === ".") {
-        args.firstArg = "0";
-    } else {
-        args.firstArg = clearTrailingDecimal(args.firstArg);
-    }
-        args.operation = operator;
-        updateDisplay();
-}
-
-//A function to clear a trailing decimal point before an operator
-function clearTrailingDecimal(arg) {
-    const trailingDecimal = /\.$/
-    if(arg.search(trailingDecimal) !== -1) {
-        return(arg.slice(0, -1));
-    } else {
-        return arg;
-    }
-}
-
-//Clear button clears display on click
-const clearButton = document.querySelector("#clear");
-clearButton.addEventListener("click", () => {
-    pushButton("clear");
-});
-
-//Equals button generates an answer on click
-//TODO: Refactor so all buttons have the same event listener
-const equalsButton = document.querySelector("#equals");
-equalsButton.addEventListener("click", () => {
-    pushButton("equals");
-});
-
-//A function to generate an answer from two args and an operator
-function generateAnswer() {
-    if(args.secondArg) {
-        args.secondArg = clearTrailingDecimal(args.secondArg);
-        args.answer = roundAnswer(operate(+args.firstArg, +args.secondArg, args.operation));
-    }
-}
-
-//A function to check whether an argument contains a decimal
-function hasDecimal(arg) {
-    const decimal = /\./;
-    if (arg.search(decimal) === -1) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
-//Decimal button inserts a decimal into the relevant argument on click
-const decimalButton = document.querySelector("#decimal");
-decimalButton.addEventListener("click", () => {
-    pushButton("decimal");
-});
+//When the decimal button is activated, push a decimal to the relevant argument and update the display.
 function pushDecimal() {
-    //If there is an answer, clear the calculator first
     if (args.answer) {
         clear();
     }
@@ -290,10 +190,53 @@ function pushDecimal() {
     updateDisplay();
 }
 
-//Round a given number such that, when converted to a string, it is no more than MAX_ARG_LENGTH characters long. 
-//Give an error message if the number exceeds the max safe value.
+//When an operator button is activated, set it as the operation to be performed. If there is an existing operator and two arguments, first perform that operation.
+function pushOperator(id) {
+    if(args.answer) {
+        answerToArg();
+    }
+    const operator = id;
+    if(args.secondArg) {
+        args.secondArg = clearTrailingDecimal(args.secondArg);
+        const answer = roundAnswer(operate(+args.firstArg, +args.secondArg, args.operation));
+        args.firstArg = answer.toString();
+        args.secondArg = null;
+    } else if (args.firstArg === ".") {
+        args.firstArg = "0";
+    } else {
+        args.firstArg = clearTrailingDecimal(args.firstArg);
+    }
+        args.operation = operator;
+        updateDisplay();
+}
+
+//When the equals button is activated, generate an answer from two arguments and an operator
+function generateAnswer() {
+    if(args.secondArg) {
+        args.secondArg = clearTrailingDecimal(args.secondArg);
+        args.answer = roundAnswer(operate(+args.firstArg, +args.secondArg, args.operation));
+    }
+}
+
+//Convert the answer from a previous operation to an argument to be operated upon
+function answerToArg() {
+    const answer = args.answer;
+    clear();
+    args.firstArg = answer.toString();
+}
+
+//Clear a trailing decimal point
+function clearTrailingDecimal(arg) {
+    const trailingDecimal = /\.$/
+    if(arg.search(trailingDecimal) !== -1) {
+        return(arg.slice(0, -1));
+    } else {
+        return arg;
+    }
+}
+
+//Round a given number such that, when converted to a string, it is no more than MAX_ARG_LENGTH characters long. Give an error message if the number exceeds the max safe value.
 function roundAnswer(ans) {
-    //If number exceeds max safe value, give an error message
     if(ans > Number.MAX_SAFE_INTEGER) {
         return "ERROR (Number too big)";
     }
@@ -303,7 +246,6 @@ function roundAnswer(ans) {
         const eIndex = ansString.indexOf("e");
         const tailLength = (eIndex === -1) ? 0 : ansString.slice(eIndex).length;
         const integralLength = decimalIndex + 1;
-        //If length up to and including decimal + length of "e notation" portion is equal to or greater than max length, simply round the answer to a whole number.
         if(integralLength + tailLength >= MAX_ARG_LENGTH) {
             return Math.round(ans);
         } else {
@@ -312,5 +254,15 @@ function roundAnswer(ans) {
         }
     } else {
         return ans;
+    }
+}
+
+//Check whether an argument contains a decimal
+function hasDecimal(arg) {
+    const decimal = /\./;
+    if (arg.search(decimal) === -1) {
+        return false;
+    } else {
+        return true;
     }
 }
